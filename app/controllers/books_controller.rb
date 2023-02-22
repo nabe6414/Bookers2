190d34fbd09d709f0cf0d58a4d8cba1ba7379acc
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-02-22 10:11:58 +0000

    books_controller記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = book.find(params[:id])
   end
 
   def edit
+    @book = book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
