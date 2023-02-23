7053591be7593f478598d6591d5acbc93912f6eb
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-02-23 07:15:38 +0000

    booksレイアウト作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,38 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
-    @book.id = current_user.id
+    @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @user = User.find(current_user.id)
+    @post = Book.new
   end
 
   def show
-    @book = book.find(params[:id])
+    @book = Book.find(params[:id])
+    @user = User.find(current_user.id)
+    @post = Book.new
   end
 
   def edit
-    @book = book.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
